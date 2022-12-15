c5b6ca1c9c5ac30f8bf0cef3acca461a11e57b38
Valeria Jiménez <vjimenezbedoya@gmail.com>
2022-12-15 13:05:51 -0500

    Terminao

--- index.js
+++ index.js
@@ -2,20 +2,18 @@ const btnNext = document.querySelectorAll(".icon-slider");
 const sliderContainer = document.querySelectorAll(".slider-movies-container");
 const btnModeColor = document.querySelector(".button-toggle");
 const elementsActives = document.querySelectorAll(
-  ".navBar,.item-categorie,.profile-text,.fa-caret-down,.container-toggle,.menuBar,.icon-menu"
+  "body,.navBar,.item-categorie,.profile-text,.fa-caret-down,.container-toggle,.menuBar,.icon-menu,.title-slider-movies"
 );
-
 let clickSlider = 1;
 
 btnNext.forEach((btn, index) => {
   btn.addEventListener("click", () => {
-    if (clickSlider === 5) {
+    console.log(sliderContainer[index].computedStyleMap().get("transform"))
+    if (clickSlider>= 5) {
       sliderContainer[index].style.transform = `translateX(0px)`;
       clickSlider = 1;
     } else {
-      sliderContainer[index].style.transform = `translateX(${
-        clickSlider * -200
-      }px)`;
+      sliderContainer[index].style.transform = `translateX(${clickSlider * -200}px)`;
       clickSlider++;
     }
   });
@@ -29,3 +27,9 @@ btnModeColor.addEventListener("click", () => {
   });
   btnModeColor.classList.toggle("active");
 });
+
+
+btnMenu.addEventListener("click",()=>{
+  categoriesMenu.classList.toggle("visited")
+
+})
